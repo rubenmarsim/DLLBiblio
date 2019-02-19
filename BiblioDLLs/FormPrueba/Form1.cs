bdfce3272fb36ccf499eb5783672522818beccc8
Rubén Martínez <43479678+rubenmarsim@users.noreply.github.com>
2019-02-19 19:09:21 +0100

    add form

---
+++ BiblioDLLs/FormPrueba/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FormPrueba
+{
+    /// <summary>
+    /// Formulario para hacer pruebas de las dlls
+    /// </summary>
+    public partial class frmPrueba : Form
+    {
+        #region Variables Globales
+
+        #endregion
+
+        #region Constructores
+        /// <summary>
+        /// Constructor por defecto
+        /// </summary>
+        public frmPrueba()
+        {
+            InitializeComponent();
+        }
+        #endregion
+
+        #region Events
+        /// <summary>
+        /// Evento que se ejecuta cuando carga la parte visual del formulario
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void frmPrueba_Load(object sender, EventArgs e)
+        {
+
+        }
+        #endregion
+
+        #region Methods
+
+        #endregion
+    }
+}
