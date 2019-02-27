7bf29762cdadaaae8d6a39b90247ea989f494497
Rubén Martínez <43479678+rubenmarsim@users.noreply.github.com>
2019-02-27 17:50:01 +0100

    add textBox Base

---
+++ BiblioDLLs/UserControls/WindowsForms/UCTextBoxBase.Designer.cs
@@ -0,0 +1,51 @@
+﻿namespace UserControls.WindowsForms
+{
+    partial class UCTextBoxBase
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de componentes
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.UCtxtBoxBase = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // UCtxtBoxBase
+            // 
+            this.UCtxtBoxBase.Location = new System.Drawing.Point(0, 0);
+            this.UCtxtBoxBase.Name = "UCtxtBoxBase";
+            this.UCtxtBoxBase.Size = new System.Drawing.Size(100, 20);
+            this.UCtxtBoxBase.TabIndex = 0;
+            this.UCtxtBoxBase.Enter += new System.EventHandler(this.UCtxtBoxBase_Enter);
+            this.UCtxtBoxBase.Leave += new System.EventHandler(this.UCtxtBoxBase_Leave);
+            this.UCtxtBoxBase.Validating += new System.ComponentModel.CancelEventHandler(this.UCtxtBoxBase_Validating);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox UCtxtBoxBase;
+    }
+}
---
+++ BiblioDLLs/UserControls/WindowsForms/UCTextBoxBase.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UserControls.WindowsForms
+{
+    /// <summary>
+    /// TextBox Base que contiene las funciones mas genericas para que los demas
+    /// TextBos puedan heredar
+    /// </summary>
+    public partial class UCTextBoxBase : TextBox
+    {
+        #region Properties
+        private bool _IsRequired;
+        /// <summary>
+        /// Si es true no deja salir del TextBox sin escribir nada
+        /// </summary>
+        public bool IsRequired
+        {
+            get { return _IsRequired; }
+            set { _IsRequired = value; }
+        }
+        private bool _ChangeColorWhenFocus;
+        /// <summary>
+        /// Cambia de color cuando recive el focus, y lo pierde a la vez que el focus
+        /// </summary>
+        public bool ChangeColorWhenFocus
+        {
+            get { return _ChangeColorWhenFocus; }
+            set { _ChangeColorWhenFocus = value; }
+        }
+        #endregion
+
+        #region Constructores
+        /// <summary>
+        /// Constructor por defecto
+        /// </summary>
+        public UCTextBoxBase()
+        {
+            InitializeComponent();
+        }
+        #endregion
+
+        #region Events
+        protected override void OnPaint(PaintEventArgs pe)
+        {
+            base.OnPaint(pe);
+        }
+        /// <summary>
+        /// Se ejecuta cuando el TextBox se esta validando
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void UCtxtBoxBase_Validating(object sender, CancelEventArgs e)
+        {
+            if (IsRequired)
+            {
+                if (Text.Length == 0)
+                {
+                    e.Cancel = true;
+                }
+            }
+        }
+        /// <summary>
+        /// Se ejecuta cuando el TextBox Recive el focus
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void UCtxtBoxBase_Enter(object sender, EventArgs e)
+        {
+            if (ChangeColorWhenFocus) BackColor = Color.LightGreen;
+        }
+        /// <summary>
+        /// Se ejecuta cuando el TextBox pierde el focus
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void UCtxtBoxBase_Leave(object sender, EventArgs e)
+        {
+            if (ChangeColorWhenFocus) BackColor = Color.White;
+        }
+        #endregion
+
+        #region Methods
+
+        #endregion
+    }
+}
